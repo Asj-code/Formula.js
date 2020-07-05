0bedbc147e917cd430bc3be2c307bcea0c5afa9f
Andrea Juarez <62590614+Asj-code@users.noreply.github.com>
2020-07-05 20:47:42 -0300

    Update index.js

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,5 @@
+//Formula to calculate a percentage of matches.
+
 var nameOne = prompt("What is your name?");
 var nameTwo =  prompt("What is your crush's name?");
 
@@ -6,3 +8,33 @@ var nameTwo =  prompt("What is your crush's name?");
     calculate = Math.floor(calculate) + 1;
 
     alert("You guys are  " + calculate + " % on each other" );
+
+// Formula to calculate the BMI
+
+function bmiCalculator (weight, height) {
+  var bmi = weight / (height * height); 
+  if (bmi < 18.5) {
+    return("Your BMI is " + bmi + ",so you are underweight");
+  } else if (bmi > 18.5 < 24.9) {
+    return("Your BMI is " + bmi + ",so you have a normal  weight");
+  } else {
+    return("Your BMI is " + bmi + ",so you are  overweight");
+  }
+  
+}
+
+bmiCalculator(60, 1.62);
+
+// Formula to calculate a leap year
+
+function isLeap(year) {
+  if(year %4 === 0 && year %400 === 0 && year %100 === 0) {
+    return("is leap year");
+  } else {
+    return("is not leap year");
+  }
+}
+
+isLeap(2400);
+
+
