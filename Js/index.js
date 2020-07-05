d0f02e20aaca5e88e6e2082ea68179e0b30c4a81
Andrea Juarez <andreasilvinaj@gmail.com>
2020-07-05 20:33:46 -0300

    First formula in Js

---
+++ Js/index.js
@@ -0,0 +1,8 @@
+var nameOne = prompt("What is your name?");
+var nameTwo =  prompt("What is your crush's name?");
+
+    var calculate = Math.random();
+    calculate = calculate * 100;
+    calculate = Math.floor(calculate) + 1;
+
+    alert("You guys are  " + calculate + " % on each other" );
